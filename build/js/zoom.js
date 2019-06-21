2b3e16da2b43ca67d6668b491471fd88d671c519
Mariya <alisa-m2@yandex.ru>
2019-06-21 11:29:40 +0300

    New zoom

--- build/js/zoom.js
+++ build/js/zoom.js
@@ -2,10 +2,6 @@ $(function(){
 $('.image__block').click(function(event) {
   var i_path = $(this).attr('src');
   $('body').append('<div id="overlay"></div><div id="magnify"><img src="img/product.png"><div id="close-popup"><i></i></div></div>');
-  $('#magnify').css({
-    left: ($(document).width() - $('#magnify').outerWidth())/2,
-    top: ($(document).height() - $('#magnify').outerHeight())/2
-  });
   $('#overlay, #magnify').fadeIn('fast');
 });
 

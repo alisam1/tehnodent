83d53d3ace87c454290c26e6fb001657bb0938b7
alisam1 <alisa-m2@yandex.ru>
2019-06-25 13:46:09 +0300

    New

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 "use strict";
 
-var gulp = require("gulp");
+var gulp = require('gulp');
 var sass = require("gulp-sass");
 var plumber = require("gulp-plumber");
 var posthtml = require("gulp-posthtml");

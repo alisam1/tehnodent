95c416375362b5c2917d496b55752ed6b328b575
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-07-18 12:45:21 +0000

    Deploy alisam1/tehnodent to github.com/alisam1/tehnodent.git:gh-pages

--- js/main.js
+++ js/main.js
@@ -8,6 +8,21 @@ jQuery(".content_toggle").click(function(){
 });
 });
 
+
+/*Mobile menu*/
+
+document.querySelector('.open-menu').addEventListener('click', () => {
+    document.querySelector('.menu_mobile').classList.add('active');
+    document.querySelector('.close-menu').classList.add('close-menu-active');
+  })
+ 
+  document.querySelector('.close-menu').addEventListener('click', () => {
+    document.querySelector('.menu_mobile').classList.remove('active');
+    document.querySelector('.close-menu').classList.remove('close-menu-active')
+})
+
+
+
 /* Sidebar */
 $('.sidebar__submenu label').on('click', function(e) {
     e.preventDefault();
